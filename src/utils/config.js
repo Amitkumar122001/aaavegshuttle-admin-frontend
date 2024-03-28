3f09096f519a460155d9f771ea8705811c437f77
Amitkumar122001 <100354931+dhruvbararia@users.noreply.github.com>
2024-03-28 13:07:50 +0530

    Amit -- pair bus driver --Assign and unAssign  driver, config.js--ip address of Aftab sir

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,10 @@
-// aftab sir [ Development ]
-export const BackendUrl = 'http://43.205.164.21:3000';
+// [ Development ]
+// export const BackendUrl = 'http://43.205.164.21:3000';
 
-// neel sir [ main ]
+// aftab sir
+export const BackendUrl = 'http://192.168.1.167:3000';
+
+// [ production ]
 // export const BackendUrl = 'http://13.200.168.251:3000';
 
 // AWS S3Bucket
